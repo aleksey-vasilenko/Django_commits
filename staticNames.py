8b044410e311627757ea0dab88291f468fc2f7a2
aleksey-vasilenko <vasilenko.aleksey1987@gmail.com>
2019-04-22 18:29:17 +0300

    add tests

---
+++ staticNames.py
@@ -0,0 +1,9 @@
+class staticXpath:
+    lastCommit = '//*[@id="js-repo-pjax-container"]/div[2]/div[1]/div[6]/div[3]/a'
+
+class staticClassName:
+    allCommits = 'BtnGroup'
+
+class staticReference:
+    commitReference = 'https://github.com/django/django/commits/master'
+    lastCommitRefernce = 'https://github.com/django/django'
---
+++ test2_django-commits.py
@@ -0,0 +1,21 @@
+import staticNames
+import workFlow
+
+from selenium import webdriver
+
+workWithClassName = workFlow.workWithSelenium(webdriver.Chrome())
+workWithXpath = workFlow.workWithSelenium(webdriver.Chrome())
+
+workWithClassName.getDriver(staticNames.staticReference.commitReference)
+commit01 = workWithClassName.findByClassName(staticNames.staticClassName.allCommits)
+
+workWithXpath.getDriver(staticNames.staticReference.lastCommitRefernce)
+commit = workWithXpath.findByXpath(staticNames.staticXpath.lastCommit)
+
+if commit[0].text == commit01[0].text:
+    print("%(expected)s equals %(actual)s" % {"expected":commit01[0].text, "actual": commit[0].text})
+else:
+    print("%(expected)s not equals %(actual)s" % {"expected": commit01[0].text, "actual": commit[0].text})
+
+workWithClassName.closeDriver()
+workWithXpath.closeDriver()
